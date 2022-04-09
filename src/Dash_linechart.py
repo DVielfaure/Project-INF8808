44ee4b9007aec0984b32cf059a666061923da1c8
Egalmi <ayoub.kassir01@gmail.com>
2022-04-09 15:12:08 -0400

    Linechart fini

--- src/Dash_linechart.py
+++ src/Dash_linechart.py
@@ -4,63 +4,54 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
+import preprocess
 import pandas as pd
 
-df = pd.read_csv("TRIP.csv")
-df['Departure Date'] = pd.to_datetime(df['Departure Date'])
-df.set_index('Departure Date', inplace=True)
-#print(df[:5][['BUSINESS_NAME', 'LATITUDE', 'LONGITUDE', 'APP_SQ_FT']])
+df = pd.read_csv("data/TRIP.csv")
 
+
+dff=preprocess.traffic_per_time(df, scale="day")
+dff = dff.groupby('Departure Day')['Traffic'].sum().reset_index()
+print(dff['Departure Day'].value_counts())
+dff["Departure Year"] = pd.to_datetime(dff["Departure Day"]).dt.year.astype('str')
+years = dff['Departure Year'].drop_duplicates().sort_values().tolist()
+print(dff[dff['Departure Year'] == '2017'].head())
+
+print(years)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
-    dcc.DatePickerRange(
-        id='my-date-picker-range',  # ID to be used for callback
-        calendar_orientation='horizontal',  # vertical or horizontal
-        day_size=39,  # size of calendar image. Default is 39
-        end_date_placeholder_text="Return",  # text that appears when no end date chosen
-        with_portal=False,  # if True calendar will open in a full screen overlay portal
-        first_day_of_week=0,  # Display of calendar when open (0 = Sunday)
-        reopen_calendar_on_clear=True,
-        is_RTL=False,  # True or False for direction of calendar
-        clearable=True,  # whether or not the user can clear the dropdown
-        number_of_months_shown=1,  # number of months shown when calendar is open
-        min_date_allowed=dt(2018, 1, 1),  # minimum date allowed on the DatePickerRange component
-        max_date_allowed=dt(2020, 6, 20),  # maximum date allowed on the DatePickerRange component
-        initial_visible_month=dt(2020, 5, 1),  # the month initially presented when the user opens the calendar
-        start_date=dt(2018, 8, 7).date(),
-        end_date=dt(2020, 5, 15).date(),
-        display_format='MMM Do, YY',  # how selected dates are displayed in the DatePickerRange component.
-        month_format='MMMM, YYYY',  # how calendar headers are displayed when the calendar is opened.
-        minimum_nights=2,  # minimum number of days between start and end date
-
-        persistence=True,
-        persisted_props=['start_date'],
-        persistence_type='session',  # session, local, or memory. Default is 'local'
-
-        updatemode='singledate'  # singledate or bothdates. Determines when callback is triggered
-    ),
-
+    
+    dcc.Dropdown(options=[{'value':year, 'label':year} for year in years], 
+            value=years[0], 
+            id='year-dropdown',
+            optionHeight=35,                    #height/space between dropdown options
+            disabled=False,                     #disable dropdown value selection
+            multi=False,                        #allow multiple dropdown values to be selected
+            searchable=True,                    #allow user-searching of dropdown values
+            search_value='',                    #remembers the value searched in dropdown
+            placeholder='Please select...',     #gray, default text shown when no option is selected
+            clearable=True,                     #allow user to removes the selected value
+            style={'width':"100%"},             #use dictionary to define CSS styles of your dropdown
+        ),
     html.H3("Evolution du trafic par mois", style={'textAlign': 'center'}),
     dcc.Graph(id='linechart')
 ])
 
 
+
 @app.callback(
     Output('linechart', 'figure'),
-    [Input('my-date-picker-range', 'start_date'),
-     Input('my-date-picker-range', 'end_date')]
+    [Input('year-dropdown', 'value')]
 )
-def update_output(start_date, end_date):
-    # print("Start date: " + start_date)
-    # print("End date: " + end_date)
-    dff = df.loc[start_date:end_date]
-    # print(dff[:5])
+def update_output(year):
 
-    fig = px.line(dff,
-        x='Departure Date',
-        y='..',
+    print('year', year)
+    dff1 = dff[dff['Departure Year'] == year].sort_values('Departure Day')
+
+    fig = px.line(dff1,
+        x='Departure Day',
+        y='Traffic',
         title=f"Evolution du traffic par mois")
                                #hover_data={'BUSINESS_NAME': True, 'LATITUDE': False, 'LONGITUDE': False,
                                         #'APP_SQ_FT': True})
