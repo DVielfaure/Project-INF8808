bbdeb9761d8e767425c17b661416da10948d1747
Quentin <quentin.parent4@gmail.com>
2022-04-21 20:19:01 -0400

    linechart ajustement

--- src/linechart.py
+++ src/linechart.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 
 
+
 def get_linechart(linechart_data,harbour=None):
 
     if harbour == None:
@@ -19,7 +20,15 @@ def get_linechart(linechart_data,harbour=None):
     fig.update_layout(
         title="Évolution du traffic sur l'année",
         title_x=0.5,
-        margin=dict(l=14, r=14, t=32, b=14, pad=0)
+        margin=dict(l=14, r=14, t=32, b=14, pad=0),
+        xaxis_title=None,
+        yaxis_title=None
+        
+        
     )
+
+    fig.update_traces(line=dict(color='rgb(13,8,135)',width= 2))
+
+    
 
     return fig
